b5579be83b1d04605176299e8816acd1bebd6bfe
Don Armstrong <don@donarmstrong.com>
2017-03-29 09:43:06 -0700

    don't bother to specify an R version

--- geo_analysis/gse_project.mk
+++ geo_analysis/gse_project.mk
@@ -18,15 +18,15 @@ ROPTS?=--no-save --no-restore -q
 GSE_FAMILES?=
 
 gse_srx_info.txt: srx_utils.R
-	$(MODULE) load R/3.2.0; \
+	$(MODULE) load R; \
 	$(R) $(ROPTS) -f $< --args samples_from_entrez $(GSE_FAMILIES) $@
 
 srx.mk: srx_utils.R gse_srx_info.txt
-	$(MODULE) load R/3.2.0; \
+	$(MODULE) load R; \
 	$(R) $(ROPTS) -f $< --args write_srx $(wordlist 2,$(words $^),$^) $@
 
 make_srx_dirs: srx_utils.R gse_srx_info.txt
-	$(MODULE) load R/3.2.0; \
+	$(MODULE) load R; \
 	$(R) $(ROPTS) -f $< --args make_srx_dirs $(wordlist 2,$(words $^),$^)
 
 get_srr: $(patsubst %,%-get_srr,$(SRX_FILES))
